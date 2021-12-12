048dadbefc18807110313a82e0105c84eec763c6
Ela0-o <90481736+Ela0-o@users.noreply.github.com>
2021-12-13 02:19:24 +0300

    Update hw1.cpp

--- hw1.cpp
+++ hw1.cpp
@@ -37,7 +37,7 @@ int main() {
   /*
   В этом блоке считаем, на какой угол
   нужно повернуть против часовой стрелки
-  вектор-точку, чтобы он стал направлен вертикально вверх
+  вектор, чтобы он стал направлен вертикально вверх
   (удобно для деления точек на левые и правые)
   */
   double a = 0.0;
@@ -69,7 +69,22 @@ int main() {
   if ((x<0) and (y>0)){
     a = 3*PI/2 - atan(x/y);
   }
-
+    
+// Теперь выясним, какие точки попали на прямую
+// middle -- точки, которые на прямой
+// dots2 -- все остальные точки. На этом моменте забываем про dots
+    vector<pair<double, double>> middle;
+    vector<pair<double, double>> dots2;
+    for (int i=0; i < dots.size(); ++i){
+        if (abs(pow((dots[i].first)*x + (dots[i].second)*y,2) - (pow(x,2) + pow(y,2))*(pow(dots[i].first,2) + pow(dots[i].second,2))) < 0.0000000001){
+            middle.push_back(dots[i]);
+        }
+        else{
+            dots2.push_back(dots[i]);
+        }
+    }
+    
+    
   /*
   В этом блоке точки делятся на левые и правые
   Величина, которую сравниваем с нулём - это
@@ -77,12 +92,12 @@ int main() {
   на матрицу поворота
   */
   vector<pair<double, double>> left, right;
-  for (int i = 0; i < dots.size(); ++i){
-    if ((cos(a)*dots[i].first - sin(a)*dots[i].second) < 0){
-      left.push_back(dots[i]);
+  for (int i = 0; i < dots2.size(); ++i){
+    if ((cos(a)*dots2[i].first - sin(a)*dots2[i].second) < 0){
+      left.push_back(dots2[i]);
     }
     else{
-      right.push_back(dots[i]);
+      right.push_back(dots2[i]);
     }
   }
 
@@ -93,7 +108,7 @@ int main() {
   (или наименьшему косинусу этого угла)
   */
   pair<double, double> l_max = left[0];
-  pair<double, double> r_max = left[0];
+  pair<double, double> r_max = right[0];
   double c = 0;
   for (int i = 0; i < left.size(); ++i){
     c = cos_betw(left[i],{x,y});
@@ -101,13 +116,23 @@ int main() {
       l_max = left[i];
     }
   }
-  for (int i = 0; i < right.size(); ++i){
-    c = cos_betw(right[i],{x,y});
-    if (cos_betw(r_max,{x,y}) > c){
-      r_max = right[i];
+  if (middle.size() == 0){
+    for (int i = 0; i < right.size(); ++i){
+        c = cos_betw(right[i],{x,y});
+        if (cos_betw(r_max,{x,y}) > c){
+        r_max = right[i];
+        }
     }
   }
-
+  else{
+    r_max = middle[0];
+    for (int i = 0; i < middle.size(); ++i){
+        c = middle[i].first*x + middle[i].second*y;
+        if (r_max.first*x + r_max.second*y > c){
+            r_max = middle[i];
+        }
+    }  
+  }
 
   cout << endl << "Leftmost: " << l_max.first << " " << l_max.second;
   cout << endl << "Rightmost: " << r_max.first << " " << r_max.second;
