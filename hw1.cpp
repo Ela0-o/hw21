11610645cc74610ed252fc07ec3f41b70a06bb6a
Ela0-o <90481736+Ela0-o@users.noreply.github.com>
2021-12-22 20:13:46 +0300

    Update hw1.cpp

--- hw1.cpp
+++ hw1.cpp
@@ -2,9 +2,10 @@
 #include <cmath>
 #include <fstream>
 
-/*
-Считаем косинус угла между двумя векторами
-*/
+
+// Считаем косинус угла между двумя векторами
+// Косинус равен склярному произведению, делённому на произведение длин
+
 float cos_betw(std::pair<double, double> a, std::pair<double, double> b)
 {
     if (a.first==0 and a.second==0){
@@ -16,26 +17,26 @@ float cos_betw(std::pair<double, double> a, std::pair<double, double> b)
 
 int main(){
     std::ifstream file ("in.txt");
-    double x = 0;
+    double x = 0; \\ x и y -- координаты заданного вектора
     double y = 0;
     file >> x >> y;
-    double xn = 0;
+    double xn = 0; \\ xn и yn -- координаты для считывания точек
     double yn = 0;
-    std::pair<double,double> left_max = {0, 0};
+    std::pair<double,double> left_max = {0, 0}; \\ эти векторы -- точки, которые мы в данный момент считаем самыми левыми и самыми правыми
     std::pair<double,double> right_max = {0, 0};
     double c = 0;
-    while (file >> xn){
+    while (file >> xn){ \\ считываем точки, пока входной поток не кончится
         file >> yn;
-        c = cos_betw({xn, yn}, {x, y});
-        if ((x*yn > xn*y) and c <= cos_betw(left_max, {x,y})){
+        c = cos_betw({xn, yn}, {x, y}); \\ сосчитали косинус угла между точкой и заданным вектором
+        if ((x*yn > xn*y) and c <= cos_betw(left_max, {x,y})){ \\ если точка слева, и косинус меньше предыдущего минимального, то записываем новую точку
             left_max = {xn, yn};
         }
-        if ((x*yn <= xn*y) and c <= cos_betw(right_max, {x,y})){
+        if ((x*yn <= xn*y) and c <= cos_betw(right_max, {x,y})){ \\ аналогично, но справа
             right_max = {xn, yn};
         }
     }
     file.close();
     
-    std::cout << std::endl << "Leftmost: " << left_max.first << " " << left_max.second;
+    std::cout << std::endl << "Leftmost: " << left_max.first << " " << left_max.second; \\ выводим самую левую и самую правую
     std::cout << std::endl << "Rightmost: " << right_max.first << " " << right_max.second;
 }
